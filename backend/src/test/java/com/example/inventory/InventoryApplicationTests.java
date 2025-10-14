9a44d2150ce4874ca73d59a94b682fedabc1f38d
ayaka910kudo <aya.caro.brbn8@gmail.com>
2025-10-14 11:26:41 +0900

    プロジェクト名をdemoからinventoryに変更

--- backend/src/test/java/com/example/inventory/InventoryApplicationTests.java
+++ backend/src/test/java/com/example/inventory/InventoryApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.inventory;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class InventoryApplicationTests {
 
 	@Test
 	void contextLoads() {
